96979757c61a775ef3c8f8b5d7db5f5bdf08b32f
Ian Macali <ianmacalinao@gmail.com>
2011-12-10 11:03:29 -0600

    Used flaggable loaders in the constructor.

--- src/main/java/com/crimsonrpg/flaggables/api/GenericFlaggableManager.java
+++ src/main/java/com/crimsonrpg/flaggables/api/GenericFlaggableManager.java
@@ -8,16 +8,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.bukkit.entity.NPC;
 
 /**
  * A generic flaggable manager.
  */
 public abstract class GenericFlaggableManager<T extends Flaggable> implements FlaggableManager<T> {
+    private final FlaggableLoader<T> loader;
     private Map<String, T> flaggables = new HashMap<String, T>();
+    
+    public GenericFlaggableManager(FlaggableLoader<T> loader) {
+        this.loader = loader;
+    }
     
     public void load(List<T> flaggables) {
         flaggables.clear();
         flaggables.addAll(flaggables);
+    }
+
+    public T create(String id) {
+        T created = loader.create(id);
+        flaggables.put(id, created);
+        return created;
     }
     
     public T get(String id) {
